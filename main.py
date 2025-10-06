4b54be75f4cab76abc2f6029fd1d6b6bbab3f088
sea-turt1e <h.yamada.bg@gmail.com>
2025-10-06 17:02:48 +0900

    add sphinx

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from talk!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ manga-graph-stapy/source/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "manga-graph-presentation-stapy"
+copyright = "2025, sea-turt1e"
+author = "sea-turt1e"
+release = "0.0.1"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx_revealjs",
+]
+
+revealjs_script_plugins = [
+    # Reveal.js組み込みのシンタックスハイライトプラグインを使う
+    {
+        "name": "RevealHighlight",
+        "src": "revealjs/plugin/highlight/highlight.js",
+    },
+    {
+        "name": "RevealNotes",
+        "src": "revealjs/plugin/notes/notes.js",
+    },
+]
+revealjs_css_files = [
+    # プラグインとセットで対応するCSSが同梱されているので、これを追加で使用
+    "revealjs/plugin/highlight/zenburn.css",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "ja"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = "alabaster"
+html_static_path = ["_static"]
